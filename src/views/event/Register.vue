2275eb7fdc7430ce803305f844df194b89414e73
Nezo96 <racradoslav96@gmail.com>
2024-05-21 17:24:48 +0200

    Added Flash Message

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { useRouter } from 'vue-router'
-const props = defineProps(['event'])
+import { inject } from 'vue'
+const { event } = defineProps(['event'])
 const router = useRouter()
+const GStore = inject('GStore')
 
 const register = () => {
   // If registration api call i successful
   // Push back to the event details
+  GStore.flashMessage = 'You are successfully registered for ' + event.title
+  setTimeout(() => {
+    GStore.flashMessage = ''
+  }, 3000)
   router.push({
     name: 'event-details',
   })
